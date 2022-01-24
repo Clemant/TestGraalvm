b8fe323f05dc25b2f9eff433bebddeb6cbfc2269
Clemant <capou.mj@gmail.com>
2022-01-24 12:05:21 -0500

    Initial commit

---
+++ src/main/java/monpackage/Test.java
@@ -0,0 +1,12 @@
+package monpackage;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Bonjour");
+
+        System.out.println(System.getProperty("os.name"));
+        System.out.println("\n");
+    }
+}
